92b8c58f5042b618634c4f9a2e6bb3382505484b
José Carlos <gkggodjihh@gmail.com>
2024-11-08 19:02:18 -0400

    Actualizar .gitignore para incluir archivos .env*.local y .env.production; corregir el título en la página de inicio a \ZOI\; agregar archivo de configuración web.config para la seguridad y redirección HTTP a HTTPS.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
         <main className="flex flex-col items-center justify-center gap-8 text-center max-w-2xl mx-auto p-8">
           <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold drop-shadow-lg animate-pulse tracking-wide">
-            Zoi
+            ZOI
           </h1>
           <p className="text-xl sm:text-2xl font-light drop-shadow-md max-w-lg leading-relaxed px-4">
             Estamos trabajando en nuestro sitio web. <br /> ¡Vuelve pronto para descubrir más!
