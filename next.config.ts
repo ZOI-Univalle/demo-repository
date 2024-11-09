2bd803caf401a569e07182024aa97cd9a18e39ea
José Carlos <gkggodjihh@gmail.com>
2024-11-08 22:38:00 -0400

    Actualizar configuración de Next.js, agregar nuevas reglas de reescritura en web.config, y mejorar el archivo .gitignore para incluir archivos .log.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  poweredByHeader: false,
+  compress: true,
+  env: {
+    NODE_ENV: 'production'
+  }
 };
 
 export default nextConfig;
