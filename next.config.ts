d942bf0e2f9310dcb5ae3940a3d0a79d510b3c6c
José Carlos <gkggodjihh@gmail.com>
2024-11-09 13:02:52 -0400

    Actualizar configuración de Next.js para deshabilitar el encabezado poweredBy, habilitar la compresión y optimizar la configuración de imágenes. Modificar reglas de reescritura en web.config para mejorar el manejo de archivos estáticos y rutas de Next.js.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    appDir: true
+  poweredByHeader: false,
+  compress: true,
+  images: {
+    unoptimized: true
   }
-}
+};
 
 export default nextConfig;
